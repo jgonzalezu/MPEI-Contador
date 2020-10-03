70445650aa01d3afe8f37b3f671ac783a51bc02f
Juan Gonzalez <Juan Gonzalez@DESKTOP-KOA4PA9>
2020-10-03 15:49:17 -0500

    Agregar botón para invertir el conteo

--- Contador_s.c
+++ Contador_s.c
@@ -1,9 +1,10 @@
 /*
- * File:   Contador.c
+ * File:   LedOn.c
  * Author: Juan Gonzalez
  *
- * Created on 23 de septiembre de 2020, 12:06
+ * Created on 20 de septiembre de 2020, 18:21
  */
+
 
 #include "reloj.h"
 #include "config.h"
@@ -14,120 +15,113 @@ void num(int dig){
     switch(dig){
         
         case 0:
-            TRISBbits.TRISB0=0;
-            TRISBbits.TRISB1=0;
-            TRISBbits.TRISB2=0;
-            TRISBbits.TRISB3=0;
-            TRISBbits.TRISB13=0;
-            TRISBbits.TRISB5=0;
-            TRISBbits.TRISB7=1;
-            break;
-            
-        case 1:
-            
-            
-            TRISBbits.TRISB0 = 1; // A
-            TRISBbits.TRISB1 = 0; // B
-            TRISBbits.TRISB2 = 0; // C
-            TRISBbits.TRISB3 = 1; // D
-            TRISBbits.TRISB13 = 1; // E
-            TRISBbits.TRISB5 = 1; // F
-            TRISBbits.TRISB7 = 1; // G
-            break;                    
-            
-        case 2:
-            TRISBbits.TRISB0=0;
-            TRISBbits.TRISB1=0;
-            TRISBbits.TRISB2=1;
-            TRISBbits.TRISB3=0;
-            TRISBbits.TRISB13=0;
-            TRISBbits.TRISB5=1;
-            TRISBbits.TRISB7=0;
-            break;
-            
-        case 3:
-            TRISBbits.TRISB0=0;
-            TRISBbits.TRISB1=0;
-            TRISBbits.TRISB2=0;
-            TRISBbits.TRISB3=0;
-            TRISBbits.TRISB13=1;
-            TRISBbits.TRISB5=1;
-            TRISBbits.TRISB7=0;
-            break;
-            
-        case 4:
-            TRISBbits.TRISB0=1;
-            TRISBbits.TRISB1=0;
-            TRISBbits.TRISB2=0;
-            TRISBbits.TRISB3=1;
-            TRISBbits.TRISB13=1;
-            TRISBbits.TRISB5=0;
-            TRISBbits.TRISB7=0;
-            break;
-            
-        case 5:
-            TRISBbits.TRISB0=0;
-            TRISBbits.TRISB1=1;
-            TRISBbits.TRISB2=0;
-            TRISBbits.TRISB3=0;
-            TRISBbits.TRISB13=1;
-            TRISBbits.TRISB5=0;
-            TRISBbits.TRISB7=0;
-            break;
-            
-        case 6:
-            TRISBbits.TRISB0=0;
-            TRISBbits.TRISB1=1;
-            TRISBbits.TRISB2=0;
-            TRISBbits.TRISB3=0;
-            TRISBbits.TRISB13=0;
-            TRISBbits.TRISB5=0;
-            TRISBbits.TRISB7=0;
-            break;
-            
-        case 7:
-            TRISBbits.TRISB0=0;
-            TRISBbits.TRISB1=0;
-            TRISBbits.TRISB2=0;
-            TRISBbits.TRISB3=1;
-            TRISBbits.TRISB13=1;
-            TRISBbits.TRISB5=1;
-            TRISBbits.TRISB7=1;
-            break;
-            
-        case 8:
-            TRISBbits.TRISB0=0;
-            TRISBbits.TRISB1=0;
-            TRISBbits.TRISB2=0;
-            TRISBbits.TRISB3=0;
-            TRISBbits.TRISB13=0;
-            TRISBbits.TRISB5=0;
-            TRISBbits.TRISB7=0;
-            break;
-            
-        case 9:
-            TRISBbits.TRISB0=0;
-            TRISBbits.TRISB1=0;
-            TRISBbits.TRISB2=0;
-            TRISBbits.TRISB3=0;
-            TRISBbits.TRISB13=1;
-            TRISBbits.TRISB5=0;
-            TRISBbits.TRISB7=0;
-            break;
-        
-        case 10:
             PORTBbits.RB0=1;
             PORTBbits.RB1=1;
             PORTBbits.RB2=1;
             PORTBbits.RB3=1;
-            PORTBbits.RB13=1;
+            PORTBbits.RB4=1;
             PORTBbits.RB5=1;
-            PORTBbits.RB7=1;
-          
+            PORTBbits.RB6=0;
+            break;
+            
+        case 1:
+            PORTBbits.RB0=0;
+            PORTBbits.RB1=1;
+            PORTBbits.RB2=1;
+            PORTBbits.RB3=0;
+            PORTBbits.RB4=0;
+            PORTBbits.RB5=0;
+            PORTBbits.RB6=0;
+            break;
+            
+        case 2:
+            PORTBbits.RB0=1;
+            PORTBbits.RB1=1;
+            PORTBbits.RB2=0;
+            PORTBbits.RB3=1;
+            PORTBbits.RB4=1;
+            PORTBbits.RB5=0;
+            PORTBbits.RB6=1;
+            break;
+            
+        case 3:
+            PORTBbits.RB0=1;
+            PORTBbits.RB1=1;
+            PORTBbits.RB2=1;
+            PORTBbits.RB3=1;
+            PORTBbits.RB4=0;
+            PORTBbits.RB5=0;
+            PORTBbits.RB6=1;
+            break;
+            
+        case 4:
+            PORTBbits.RB0=0;
+            PORTBbits.RB1=1;
+            PORTBbits.RB2=1;
+            PORTBbits.RB3=0;
+            PORTBbits.RB4=0;
+            PORTBbits.RB5=1;
+            PORTBbits.RB6=1;
+            break;
+            
+        case 5:
+            PORTBbits.RB0=1;
+            PORTBbits.RB1=0;
+            PORTBbits.RB2=1;
+            PORTBbits.RB3=1;
+            PORTBbits.RB4=0;
+            PORTBbits.RB5=1;
+            PORTBbits.RB6=1;
+            break;
+            
+        case 6:
+            PORTBbits.RB0=1;
+            PORTBbits.RB1=0;
+            PORTBbits.RB2=1;
+            PORTBbits.RB3=1;
+            PORTBbits.RB4=1;
+            PORTBbits.RB5=1;
+            PORTBbits.RB6=1;
+            break;
+            
+        case 7:
+            PORTBbits.RB0=1;
+            PORTBbits.RB1=1;
+            PORTBbits.RB2=1;
+            PORTBbits.RB3=0;
+            PORTBbits.RB4=0;
+            PORTBbits.RB5=0;
+            PORTBbits.RB6=0;
+            break;
+            
+        case 8:
+            PORTBbits.RB0=1;
+            PORTBbits.RB1=1;
+            PORTBbits.RB2=1;
+            PORTBbits.RB3=1;
+            PORTBbits.RB4=1;
+            PORTBbits.RB5=1;
+            PORTBbits.RB6=1;
+            break;
+            
+        case 9:
+            PORTBbits.RB0=1;
+            PORTBbits.RB1=1;
+            PORTBbits.RB2=1;
+            PORTBbits.RB3=1;
+            PORTBbits.RB4=0;
+            PORTBbits.RB5=1;
+            PORTBbits.RB6=1;
             break;
     }
     
 
+}
+
+int pulsador(){
+    int dir;
+    dir = PORTBbits.RB9;
+    return dir;
 }
 
 int main(void) {
@@ -138,48 +132,62 @@ int main(void) {
     TRISBbits.TRISB1 = 0; // B
     TRISBbits.TRISB2 = 0; // C
     TRISBbits.TRISB3 = 0; // D
-    TRISBbits.TRISB13 = 0; // E
+    TRISBbits.TRISB4 = 0; // E
     TRISBbits.TRISB5 = 0; // F
-    TRISBbits.TRISB7 = 0; // G
-    
-    
+    TRISBbits.TRISB6 = 0; // G
     
     TRISBbits.TRISB8 = 0; // 1 DIGITO
-    TRISBbits.TRISB9 = 0; // 2 DIGITO
+    TRISBbits.TRISB7 = 0; // 2 DIGITO
     
-     
+    TRISBbits.TRISB9 = 1; // PULSADOR
+      
     
-    int tiempo = 50;
-    int del = 1;
-    
+    int tiempo = 100;
+    int del = 1 ;
     while(1){
         
-
         int i;
         int j;
         int t;
+        int dir;
         
-        for(i = 0; i < 10; i++){ 
-            for(j = 0; j < 10; j++){
-                
+        for(i = 0; i < 10 ; i = i){
+            for(j = 0; j < 10 ; j = j){  
                 for(t = 0; t < tiempo; t++){
-                
+
                     
-                    PORTBbits.RB9 = 0;
-                    __delay_ms(del)
                     PORTBbits.RB8 = 1;
-                    num(j);
-                    __delay_ms(del)
+                    PORTBbits.RB7 = 0;
                     
-                    PORTBbits.RB8 = 0;
-                    __delay_ms(del)
-                    PORTBbits.RB9 = 1;
                     num(i);
-                    __delay_ms(del)
+
+                    __delay_ms(del);
+                    
+                    PORTBbits.RB7 = 1;
+                    PORTBbits.RB8 = 0;
+                    num(j);
+
+                    __delay_ms(del);
+            
                 }
-            }                  
+                
+                dir = pulsador();
+                
+                if (dir == 1){
+                    j++;
+                } else {
+                    j--;
+                }
+            }
+            
+            dir = pulsador();
+            
+            if (dir == 1){
+                i++;
+            } else {
+                i--;
+            }
         }
+ 
     }  
 }
-
-
